81a99cd48752aa81e7315ca65552ff4c20049f49
thomas.ferns <thomasferns@encompasscorporation.com>
2021-12-07 13:12:33 +0000

    Update day 7

--- solutions/aoc_d7.py
+++ solutions/aoc_d7.py
@@ -3,24 +3,28 @@ puzzle = [int(x) for x in puzzle]
 
 bot, top = min(puzzle), max(puzzle)
 
-def findFuelUsed(num):
-    total = 0
-    for x in range(1, num+1):
-        total += x
-    return total
+def findFuelUsed(num, better=False):
+    if not better:
+        total = 0
+        for x in range(1, num+1):
+            total += x
+        return total
+    return int(((num*num) + num) / 2)
 
-def solve(part2=False):
+def solve(part2=False, better=False):
     fuel_counts = [0 for _ in range(bot, top)]
     for x in range(bot, top):
         for pos in puzzle:
             distance = abs(pos - (x+1))
-            fuel_counts[x] += findFuelUsed(distance) if part2 else distance
-    return min(fuel_counts)
+            fuel_counts[x] += findFuelUsed(distance, better) if part2 else distance
+    return min(fuel_counts), fuel_counts.index(min(fuel_counts)) + 1
 
-part1ans = solve()                   
-print(f'part 1: {part1ans}')
+part1ans, median = solve()         
+print(f'part 1: {part1ans, median}')
 
-part2ans = solve(part2=True)
-print(f'part 2: {part2ans}')
-                           
+part2ans, mean = solve(part2=True, better=True)
+print(f'part 2 better: {part2ans, mean}')
+
+part2ans, mean = solve(part2=True)
+print(f'part 2: {part2ans, mean}')                           
             
