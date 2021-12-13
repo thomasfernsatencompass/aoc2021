7a1cf6826b94116efcaf5aaddcaf25190fea1167
thomas.ferns <thomasferns@encompasscorporation.com>
2021-12-13 13:10:57 +0000

    Add day 10

--- solutions/aoc_d10.py
+++ solutions/aoc_d10.py
@@ -1,5 +1,4 @@
-puzzle="""
-[({(<(())[]>[[{[]{<()<>>
+puzzle="""[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
 {([(<{}[<>[]}>{[]{[(<()>
 (((({<>}<{<{<>}{[]{[]{}
@@ -8,8 +7,37 @@ puzzle="""
 {<[[]]>}<{[{[{[]{()[[[]
 [<(<(<(<{}))><([]([]()
 <{([([[(<>()){}]>(<<{{
-<{([{{}}[<[[[<>{}]]]>[]]
-""".splitlines()
-puzzle = [x for x in puzzle if x]
+<{([{{}}[<[[[<>{}]]]>[]]""".splitlines()
 
-#todo
+openers = {'(':')', '[':']', '{':'}', '<':'>'}
+points = {')':3, ']':57, '}':1197, '>':25137}
+
+errors = []
+remainingStacks = []
+for line in puzzle:
+    openStack = []
+    hasError = False
+    for char in line:
+        if char in openers:
+            openStack.append(char)
+        else:
+            latest = openStack.pop()
+            if openers[latest] != char:
+                hasError = True
+                errors.append(char)
+    if not hasError:
+        remainingCloses = [openers[o] for o in openStack[::-1]]
+        remainingStacks.append(remainingCloses)
+part1ans = sum([points[e] for e in errors])
+print(f'part 1: {part1ans}')
+
+autocompleteScore = {')':1, ']':2, '}':3, '>':4}
+stackScores = []
+for stack in remainingStacks:
+    stackScore = 0
+    for char in stack:
+        stackScore *= 5
+        stackScore += autocompleteScore[char]
+    stackScores.append(stackScore)
+middleScore = sorted(stackScores)[int(len(stackScores)/2)]
+print(f'part 2: {middleScore}')   
